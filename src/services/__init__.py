befce8634d18c2c9fe180a9898245ef73baad6bf
erinma6 <erinma6@gmail.com>
2026-04-12 12:29:38 -0400

    Fix broken import of deleted enrichment module in services __init__

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,9 +1,6 @@
-from .enrichment import EnrichmentService, RestaurantEnrichment
 from .search import SearchService, SearchFilters, QueryParser
 
 __all__ = [
-    "EnrichmentService",
-    "RestaurantEnrichment",
     "SearchService",
     "SearchFilters",
     "QueryParser",
