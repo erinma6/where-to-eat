95b30f69907a0b6fc69183bb369fd506547d1faa
erinma6 <erinma6@gmail.com>
2026-04-12 12:31:29 -0400

    Remove dead google_places import from api __init__

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,9 +1,6 @@
-from .google_places import GooglePlacesClient, fetch_place_with_reviews
 from .openai_client import OpenAIClient, SyncOpenAIClient
 
 __all__ = [
-    "GooglePlacesClient",
-    "fetch_place_with_reviews",
     "OpenAIClient",
     "SyncOpenAIClient",
 ]
